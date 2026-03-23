694f9c2db3ec07cb8c6ed6bb8d22a4f22c11c673
kkkorkor <kwankaow2112@gmail.com>
2026-03-23 09:49:22 +0000

    TvT

--- student.h
+++ student.h
@@ -5,10 +5,33 @@ class student{
 private:
 
   string name;
+  int age;
 
 public:
   
    void set_name(string="Yme");
-    void print_name();
+   void print_name();
+   void set_age(int);
+   int get_age();
+
+   //1.set_age --> set ค่า age
+   //2.get_age --> return age
 
 };
+
+void student::set_age(int a){
+  age=a;
+}
+
+int student::get_age(){
+  return age;
+}
+
+void student::set_name(string n){
+  name=n;
+}
+
+void student::print_name(){
+  cout<<"Name: "<<name<<endl;
+}
+
